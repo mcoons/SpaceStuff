9a7c3c790abc5bdae562f5abe0e032c2c695d079
mcoons <mcoons67@gmail.com>
2021-03-15 01:54:58 -0600

    upgrading

--- src/app/hubble-images/hubble-images.component.spec.ts
+++ src/app/hubble-images/hubble-images.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HubbleImagesComponent } from './hubble-images.component';
 
@@ -6,7 +6,7 @@ describe('HubbleImagesComponent', () => {
   let component: HubbleImagesComponent;
   let fixture: ComponentFixture<HubbleImagesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HubbleImagesComponent ]
     })
--- src/app/hubble-news/hubble-news.component.spec.ts
+++ src/app/hubble-news/hubble-news.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HubbleNewsComponent } from './hubble-news.component';
 
@@ -6,7 +6,7 @@ describe('HubbleNewsComponent', () => {
   let component: HubbleNewsComponent;
   let fixture: ComponentFixture<HubbleNewsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HubbleNewsComponent ]
     })
